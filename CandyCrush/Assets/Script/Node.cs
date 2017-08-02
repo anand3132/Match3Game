c70ad6855704dccbc5826c68aa56e63ab0d5365a
anand <anand.mudhaliar@gmail.com>
2017-08-02 18:33:51 +0530

    Linking done

---
+++ CandyCrush/Assets/Script/Node.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node:MonoBehaviour{
+	public int ID;
+	Vector3 nodePosition;
+
+	public Node leftNode;
+	public Node rightNode;
+	public Node topNode;
+	public Node BottomNode;
+	public Node topRightNode;
+	public Node topLeftNode;
+	public Node BottomRightNode;
+	public Node BottomLeftNode;
+
+	public Node next;
+	public Node(){
+		nodePosition = gameObject.transform.position;
+	}
+
+}
+
+
--- CandyCrush/Assets/Script/TileGenerator.cs
+++ CandyCrush/Assets/Script/TileGenerator.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile{
+	//pointer to actual tile
 	public GameObject tileObject;
-	public string type;
+	public string id;
+	public int col,row;
+	public Tile leftNode;
+	public Tile rightNode;
+	public Tile topNode;
+	public Tile BottomNode;
+	public Tile topRightNode;
+	public Tile topLeftNode;
+	public Tile BottomRightNode;
+	public Tile BottomLeftNode;
+
 	public Tile(GameObject tile,string t){
 		tileObject = tile;
-		type = t;
+		id = t;
 	}
 }
 
@@ -23,21 +34,21 @@ public class TileGenerator : MonoBehaviour {
 
 	static int cols=8;
 	static int rows = 8;
-	int objectsToBePooled=(rows * cols) / 3;
+	int objectsToBePooled=(rows * cols) / 4;
 	bool renewGrid = false;
 
 	void Start () {
 		ObjectPool (objectsToBePooled);
 		ShuffleList (tileBank);
 		GenerateTileGrid ();
+		LinkTileGrid ();
 	}
 
 	void ObjectPool(int poolSize){
 		//object pooling
 		for (int i = 0; i < poolSize; i++) {
 			for (int j = 0; j < tile.Length; j++) {
-				GameObject o = (GameObject)Instantiate (tile [j], 
-					new Vector3 (-10, -10, 0), tile [j].transform.rotation);
+				GameObject o = (GameObject)Instantiate (tile [j], new Vector3 (-10, -10, 0), tile [j].transform.rotation);
 				o.SetActive (false);
 				tileBank.Add (o);
 			}
@@ -66,20 +77,77 @@ public class TileGenerator : MonoBehaviour {
 						o.transform.position = new Vector3 (tilePos.x, tilePos.y, tilePos.z);
 						o.SetActive (true);
 						tiles[c, r] = new Tile (o, o.name);
+						tiles [c, r].col = c;
+						tiles [c, r].row = r;
 						n = tileBank.Count + 1;
 					}
 				}
 			}
 		}
 	}
+	void LinkTileGrid(){
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < cols; c++) {
+				if (c == 0) { 
+					tiles [c, r].topLeftNode = null;
+					tiles [c, r].leftNode = null;
+					tiles [c, r].BottomLeftNode = null;
+				} else {
+					tiles [c, r].leftNode = tiles [c-1, r];
+					if (r == 0) {
+						tiles [c, r].BottomLeftNode = null;
+					} else {
+						tiles [c, r].BottomLeftNode = tiles [c - 1, r - 1];
+					}
+					if (r == rows - 1) {
+						tiles [c, r].topLeftNode = null;
+					} else {
+						Debug.Log(" c "+c+" r "+r);
+						tiles [c, r].topLeftNode = tiles [c - 1, r + 1];
+					}
+				}
+
+				if (c == cols - 1) {
+					tiles [c, r].topRightNode = null;
+					tiles [c, r].rightNode = null;
+					tiles [c, r].BottomRightNode = null;				
+				} else {
+					tiles [c, r].topRightNode = tiles [c + 1, r + 1];
+					tiles [c, r].rightNode = tiles [c+1, r ];
+					if (r == 0) {
+						tiles [c, r].BottomRightNode = null;
+					} else {
+						tiles [c, r].BottomRightNode = tiles [c + 1, r + 1];
+					}
+					
+				}
+				if (r == 0) {
+					tiles [c, r].BottomNode = null;
+				} else {
+					tiles [c, r].BottomNode = tiles [c , r-1];
+
+				}
+				if (r == rows - 1) {	
+					tiles [c, r].topNode = null;
+				} else {
+					tiles [c, r].topNode = tiles [c , r+1];
+
+				}	
+			}//for (col)
+		}//for (row)
+	}//LinkTileGrid()
+
 	// Update is called once per frame
 	void Update () {
-		
+
+
 		if (Input.GetMouseButtonDown (0)) {
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit2D hit = Physics2D.GetRayIntersection (ray, 1000);
 			if (hit) {
 				tile1 = hit.collider.gameObject;
+
+
 			}
 		} else if(Input.GetMouseButtonUp(0)&&tile1) {
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
@@ -89,11 +157,11 @@ public class TileGenerator : MonoBehaviour {
 			}
 
 			if (tile1 && tile2) {
+				
 				int tile1posX = (int)tile1.transform.position.x;
 				int tile1posY = (int)tile1.transform.position.y;
 				int tile2posX = (int)tile2.transform.position.x;
 				int tile2posY = (int)tile2.transform.position.y;
-					
 					
 				int horDist = (int)Mathf.Abs (tile1posX - tile2posX);
 				int verDist = (int)Mathf.Abs (tile1posY - tile2posY);
@@ -107,57 +175,96 @@ public class TileGenerator : MonoBehaviour {
 				Vector3 tempPos = tile1.transform.position;
 				tile1.transform.position = tile2.transform.position;
 				tile2.transform.position = tempPos;
+					Debug.Log("Colum "+ tiles [tile1posX, tile1posY].col+"Row "+
+						tiles [tile1posX, tile1posY].row);
+					
 				tile1 = null;
 				tile2 = null;
+
 				}
 			}
 		}
 		MatchTile ();
 	}
 	void MatchTile(){
+		Debug.Log ("Match Called");
 		int counter = 1;
 		for (int r = 0; r < rows; r++) {
 			counter = 1;
 			for (int c = 1; c < cols; c++) {
 				if (tiles [c, r] != null && tiles [c - 1, r] != null) {
-					if (tiles [c, r].type == tiles [c - 1, r].type) {
+					if (tiles [c, r].id == tiles [c - 1, r].id) {
 						counter++;
+						if (counter == 3 && (c- 3)>0 && (c+1)<=cols) {
+							if (tiles [c-3, r] != null && tiles [c+1 - 1, r] != null) {
+//								if (tiles [c, r].id == tiles [c - 3, r].id ^ tiles [c, r].id == tiles [c + 1, r].id) {
+//									counter++;
+//									Debug.Log ("Foure colum   match found");
+//								}
+							}
+						}
+				
 					} else
 						counter = 1;
-					if (counter == 3) {
+					
+					if (counter >= 3) {
 						if (tiles [c, r] != null)
 							tiles [c, r].tileObject.SetActive (false);
 						if (tiles [c - 1, r] != null)
 							tiles [c - 1, r].tileObject.SetActive (false);
 						if (tiles [c - 2, r] != null)
 							tiles [c - 2, r].tileObject.SetActive (false);
+						
+						if (counter ==4) {
+							if (tiles [c - 3, r] != null)
+								tiles [c - 3, r].tileObject.SetActive (false);
+							tiles [c - 3, r] = null;
+						}
+
 						tiles [c, r] = null;
 						tiles [c - 1, r] = null;
 						tiles [c - 2, r] = null;
 						//renewBoard = true;
 					}
 				}
-			}
-		}
+			}//for(colum)
+		}//for(row)
 		//row check
 		for (int c = 0; c < cols; c++){
 			counter=1;
 			for(int r=1;r<rows;r++){
 				if (tiles [c, r] != null && tiles [c , r-1] != null) {
-					if (tiles [c, r].type == tiles [c, r-1].type) {
+					if (tiles [c, r].id == tiles [c, r-1].id) {
 						counter++;
+						if (counter ==  3 && (r- 3)>0 && (r+1)<=rows) {
+							if (tiles [c, r - 3] != null && tiles [c, r + 1] != null) {
+								if (tiles [c, r].id == tiles [c, r - 3].id ^ tiles [c, r].id == tiles [c, r + 1].id) {
+									counter++;
+									Debug.Log ("Foure row  match found");
+								}
+							}
+						}
+
 					} else
 						counter = 1;
-					if (counter == 3) {
+					if (counter >= 3) {
 						if (tiles [c, r] != null) 
 							tiles [c, r].tileObject.SetActive (false);
 						if (tiles [c , r-1] != null)
 							tiles [c , r-1].tileObject.SetActive (false);
 						if (tiles [c , r-2] != null)
 							tiles [c , r-2].tileObject.SetActive (false);
+						
+						if (counter == 4) {
+							if (tiles [c, r - 3] != null)
+								tiles [c, r - 3].tileObject.SetActive (false);
+							tiles [c, r-3] = null;
+						}
+
 						tiles [c, r] = null;
 						tiles [c, r-1] = null;
 						tiles [c, r-2] = null;
+
 						renewGrid = true;
 					}
 				}
@@ -168,6 +275,7 @@ public class TileGenerator : MonoBehaviour {
 			renewGrid = false;
 			Debug.Log ("True");
 		}
+		//RenewGrid ();
 	}//MatchTile()
 	void RenewGrid (){
 		bool tileMoved = false;
